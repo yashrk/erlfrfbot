069817b560c3c54bf4c7dd5153da78b0aef61be1
Yuriy Shirokov <yushi@altlinux.org>
2016-04-10 01:33:39 +0300

    Function to post current date

--- src/utils.erl
+++ src/utils.erl
@@ -1,5 +1,5 @@
 -module(utils).
--export([russian_month/1]).
+-export([russian_month/1, post_current_date/0]).
 
 russian_month(1)->
     <<"января"/utf8>>;
@@ -25,3 +25,11 @@ russian_month(11)->
     <<"ноября"/utf8>>;
 russian_month(12)->
     <<"декабря"/utf8>>.
+
+post_current_date() ->
+    {_Year, Month, Day} = date(),
+    DayBin = unicode:characters_to_binary(io_lib:format("~tp ", [Day])),
+    MonthBin = russian_month(Month),
+    RussianDate = <<DayBin/binary, MonthBin/binary>>,
+    io:format("~tp~n", [RussianDate]),
+    frfbot:post(RussianDate).
